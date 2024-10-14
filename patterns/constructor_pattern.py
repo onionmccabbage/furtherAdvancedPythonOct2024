ce865c5be1b320a48fde1c70585dcffc11a06222
onionmccabbage <onionmccabbage@live.com>
2024-10-14 10:45:35 +0100

    a rigorous class constructing a planar point x y validated as numbers

--- patterns/constructor_pattern.py
+++ patterns/constructor_pattern.py
@@ -15,7 +15,8 @@ class Point(): # or class (object): or class Point:
     __slots__ = ('__x', '__y') # constrain this class to allow only these properties
     def __init__(self, x, y):
         '''a bit like a constructor'''
-        self.x = x
+        # here self.x looks like a property, but Python calls a method (passin the right-side of the property setter)
+        self.x = x # here we call the SETTER METHOD (only works due to @property and @setter)
         self.y = y
     @property
     def x(self):
@@ -36,8 +37,26 @@ class Point(): # or class (object): or class Point:
         if type(new_y) in (int, float):
             self.__y = new_y
         else:
-            raise TypeError('T must be numeric')
+            raise TypeError('Y must be numeric')
+    # we may also declare class methods
+    def __str__(self):
+        '''this method overrides the built in __str__ and is used by 'print()' '''
+        return f'This point is at {self.x} {self.y} hypot {self.hypot()}'
+    def __repr__(self):
+        '''override the built in __repr__ used in immediate Python'''
+        return f'Point at {self.x} {self.y}'
+    def hypot(self): # here we declare a derived property (does not persist in the class)
+        '''the hypotenuse is derived from the square root of the sum of the squares of thx and y'''
+        h = (self.x**2+self.y**2)**0.5 # call the getter methods for __x and __y
+        return h
 
 if __name__ == '__main__':
     '''we may instantiate our class'''
-    p1 = Point(4,3)
+    p1 = Point(4,3) # this will call the property setter for x and for y, thereby validating x and y
+    # p2 = Point('4', 3)
+    p1.x = 5 # all good - when we mutate properties it calls the setter method
+    # p1.y = [] # nope - this raises the typeError
+    print( p1.x, p1.y)
+    print(p1)
+
+
