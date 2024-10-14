2a05772d1fa25bbd43622265db7665ff4aef9ee9
onionmccabbage <onionmccabbage@live.com>
2024-10-14 13:40:01 +0100

    text for review exercise 1

--- patterns/my_decorator.py
+++ patterns/my_decorator.py
@@ -6,7 +6,8 @@
 def showArgs(func): # take a function as an argument
     '''this function may be used to decorate any other function
     It will reveal all arguments and positional arguments
-    Then run the original function'''
+    Then run the original function
+    This is flexible for ANY number of args/kwargs'''
     def newFunc(*args, **kwargs):
         '''log the arg/kwarg values'''
         print(f'''Running a function called {func.__name__}
@@ -16,19 +17,27 @@ The keyword arguments are {kwargs}''')
         return func(*args, **kwargs)
     return newFunc # we do no invoke our new function
 
+# CAREFUL - every decorator operates on the IMMEDIATELY FOLLOWING function
 @showArgs # we apply our decorator to this function
 def isOdd(n):
     return n%2 !=0
 
-@showArgs
+@showArgs    
 def squares(m,n):
     s = []
     for i in range(m,n):
         s.append(i*i)
     return s
 
+@showArgs
+def lunch(*args, **kwargs): # we may expect zero or more arguments
+    '''we may expect no arguments or lots of them'''
+    return 'done'
+
 if __name__ == '__main__':
     print( isOdd(7) ) # True
     print( squares(-2, 3) ) # [4,1,0,1,4]
     # call a function with keyword arguments
     print( squares(m=-2, n=3) ) # [4,1,0,1,4]
+    lunch() # no args/kwargs
+    lunch(3,4,5,6,7,8,9)
