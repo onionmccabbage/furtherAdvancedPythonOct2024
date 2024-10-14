a50f3b96b019fb2d4a600c5d0ed448a22263d035
onionmccabbage <onionmccabbage@live.com>
2024-10-14 16:05:34 +0100

    sample example for proxy design pattern

--- patterns/prxy_demo/debit_card.py
+++ patterns/prxy_demo/debit_card.py
@@ -5,6 +5,7 @@ class DebitCard(Payment):
     def __init__(self):
         self.bank = Bank()
     def doPay(self):
-        card = input('swipe, tap, insert...? ')
+        # card = input('swipe, tap, insert...? ')
+        card = 'swipe'
         self.bank.setCard(card)
-        return self.bank.doPay # True or False
+        return self.bank.doPay() # True or False
