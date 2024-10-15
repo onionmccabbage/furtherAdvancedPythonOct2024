256ce5f211ad269e7e2ba809547f7b12bcc5d13b
onionmccabbage <onionmccabbage@live.com>
2024-10-15 10:05:29 +0100

    stateful design pattern

--- patterns/using_state/_computer_state.py
+++ patterns/using_state/_computer_state.py
@@ -4,5 +4,6 @@ class ComputerState():
     def switch(self, new_state):
         if new_state.name in self.allowed:
             print(f'Current state {self} switching to {new_state}')
+            self.__class__ = new_state # actually make the state change
         else:
             print(f'Current state {self} cannot switch to {new_state}')
