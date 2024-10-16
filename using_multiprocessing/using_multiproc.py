43a4e1b1a701748495aa7da7e185040e65ce4202
onionmccabbage <onionmccabbage@live.com>
2024-10-16 12:18:34 +0100

    using multiprocessing

--- using_multiprocessing/using_multiproc.py
+++ using_multiprocessing/using_multiproc.py
@@ -6,18 +6,18 @@ value = 'this is global'
 
 def whichProc(n):
     '''see which process ID this code is running on'''
-    global value
-    value  = n
+    global value # this will be a global scope within each separate process
+    value  = n # careful - each new process will copy the original parent process scope
     print( f'Running on process ID {os.getpid()} value is {value}' )
 
 if __name__ == '__main__':
     #we can cmpare running on ONE process or many
-    whichProc(0) # this copy of Python is running on a process
+    # whichProc(0) # this copy of Python is running on a process
     start1 = timeit.default_timer()
-    whichProc(1) 
-    whichProc(2)
-    whichProc(3)
-    whichProc(4)
+    # whichProc(1) 
+    # whichProc(2)
+    # whichProc(3)
+    # whichProc(4)
     end1 = timeit.default_timer()
     print(f'Sequential took {end1 - start1}')
     # next we make a list of separate processes
@@ -31,6 +31,9 @@ if __name__ == '__main__':
         p.start() # this is where the process begins to run
     for _ in procs_l:
         _.join() # optional but good practice
+        print(value) # is this value inside the process?
+    print(value) # this is the orignal global object
+    
     
     end2 = timeit.default_timer()
     print(f'Parallel took {end2 - start2}')
