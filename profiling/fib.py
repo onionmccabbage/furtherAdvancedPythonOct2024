5ed80632654d3b92a35a21422b9c6e213300957d
onionmccabbage <onionmccabbage@live.com>
2024-10-16 13:44:18 +0100

    using fibonacci

--- profiling/fib.py
+++ profiling/fib.py
@@ -3,10 +3,9 @@
 from memory_profiler import profile 
 import timeit
 
-
 def fib1(n):
     '''Here is a low performance Fibonacci function'''
-    if n==1:
+    if n in (0, 1): # if n>=1
         return 1
     else:
         '''recursively call this function again'''
@@ -16,8 +15,9 @@ def fib2(n):
     ''''''
 
 if __name__ == '__main__':
+    print('fib')
     start1 = timeit.default_timer()
-    print( fib1(4) ) # find the result of the first 4 members of the sequence
+    print( fib1(7) ) # find the result of the first 4 members of the sequence
     end1 = timeit.default_timer()
     print(f'total: {end1-start1}')
     # start2 = timeit.default_timer()
