2496d0649aec3479c794d04fb0eed331c4169fda
onionmccabbage <onionmccabbage@live.com>
2024-10-18 10:26:45 +0100

    simple socket client

---
+++ microservices/client.py
@@ -0,0 +1,16 @@
+import socket
+
+def client():
+    '''This simple socket client will make requests over IP'''
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    setup_t=('localhost', 9876)
+    client.connect(setup_t) # we connect to the server
+    msg = 'hello'
+    client.send(msg.encode())
+    # we may reasonably expect a response
+    response = client.recv(1024)
+    print(f'Client received {response}') # response will be bytes
+    client.close()
+
+if __name__ == '__main__':
+    client()
